c6dd28929bb5f020fd77147803fae90a04e36363
Jonathan Wambua <jonathanwambua10219@gmail.com>
2022-05-30 22:08:59 +0300

    register function updated

--- php/register.php
+++ php/register.php
@@ -10,9 +10,16 @@ registerUser($username, $email, $password);
 
 function registerUser($username, $email, $password){
     //save data into the file
-    
+    $data = array("username"=>$username, "email"=>$email, "password"=>$password);
+    $file = fopen('../storage/users.csv', 'a');
+    if(fputcsv($file, $data)){
+        echo "User Successfully registered";
+    };
+    fclose($file);
+
     // echo "OKAY";
+    echo "<br> OKAY";
 }
-echo "HANDLE THIS PAGE";
+echo "<br> HANDLE THIS PAGE";
 
 
